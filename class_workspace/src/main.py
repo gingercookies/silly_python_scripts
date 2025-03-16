31da85ae525d33bb63e606483ff1f870776bb14b
gingercookies <dolybao2006@gmail.com>
2025-03-16 23:45:46 +0700

    Cleanup, rearrange and try to catch some errors

--- class_workspace/src/main.py
+++ class_workspace/src/main.py
@@ -1,10 +1,8 @@
 import os; from datetime import datetime as dt
 executable_dir = os.path.dirname(os.path.realpath(__file__))
-print(executable_dir)
-
 os.chdir(executable_dir)
 current_date = dt.now()
-print(current_date)
+
 
 def custom_time():
     form = int(input(f"""Format?:
@@ -25,7 +23,7 @@ def custom_time():
 def path_resolver(date):
     "Return new folder path with current date in its name"
     new_folder_name = date.strftime("%B_%d_%Y")
-    new_folder_path = executable_dir + "/" + new_folder_name
+    new_folder_path = os.path.join(executable_dir,new_folder_name)
     return new_folder_path
 
 def create_new_folder(path):
@@ -34,43 +32,51 @@ def create_new_folder(path):
         print(f"Directory created: {path}")
     except FileExistsError:
         print(f"Directory already exist: {path}")
-
-def mode_selector():
-    if mode == 3:
-        print("Exiting")
+    except PermissionError:
+        print(f"Permission denied while creating the directory {path}.")
+    except FileNotFoundError:
+        print(f"Folder not found: {path}.\nEXITTING!")
         exit()
-    elif mode == 2:
-        path = path_resolver(custom_time())
-        create_new_folder()
-    elif mode == 1:
-        path = path_resolver(current_date)
-        create_new_folder(path)
-        symlink_boiii(path,current_date)
-    else:
-        print("Invalid, try again.")
-        mode_selector()
+        
 
 def symlink_boiii(path,date):
-    new_folder_name = date.strftime("%B_%d_%Y")
-    for i in ["Homework","Classwork"]:
-        src_path = os.path.join(executable_dir, os.pardir, i,new_folder_name)
-        destination_path = os.path.join(path,i)
+    subfolder_name = date.strftime("%B_%d_%Y")
+    for folder_name in ["Homework","Classwork"]:
+        src_path = os.path.join(executable_dir, os.pardir, folder_name, subfolder_name)
+        destination_path = os.path.join(path,folder_name)
         
         try:
             create_new_folder(src_path)
-        except:
+        except FileExistsError:
             pass
         
         try:
             os.symlink(src_path,destination_path)
         except FileExistsError:
             print(f"Symlink already exist: {destination_path}")
+        except PermissionError:
+            print(f"Permission denied while creating the fsymlink {destination_path}.")
 
-global mode
-mode = int(input(f"""Hello, {os.getlogin()}
-We 're currently in {executable_dir}
-What do you want to do?
-1. Create a new folder with current datetime ({current_date})
-2. Create a new folder with custom datetime (MMMM dd yyyy)
-3. Exit\n"""))
-mode_selector()
+
+def main():
+    mode = int(input(f"""Hello, {os.getlogin()}
+    We 're currently in {executable_dir}
+    What do you want to do?
+    1. Create a new folder with current datetime ({current_date})
+    2. Create a new folder with custom datetime (MMMM dd yyyy)
+    3. Exit\n"""))
+    if mode == 3:
+        print("Exiting")
+        exit()
+    elif mode == 2:
+        path = path_resolver(custom_time())
+        create_new_folder(path)
+    elif mode == 1:
+        path = path_resolver(current_date)
+        create_new_folder(path)
+        symlink_boiii(path,current_date)
+    else:
+        print("Invalid, try again.")
+
+if __name__ == "__main__":
+    main()
